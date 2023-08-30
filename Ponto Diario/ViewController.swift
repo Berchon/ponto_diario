2acb0eb64b61d8261b976f7bdb2b6f4c48fd2e28
LucianoBerchon-Meli <luciano.berchon@mercadolivre.com>
2023-08-30 16:58:24 -0300

    Initial draft of mobile app screen layout

---
+++ Ponto Diario/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ponto Diario
+//
+//  Created by Luciano Da Silva Berchon on 30/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
